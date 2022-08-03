2e794893eb439908ba7fe8a1d3ddc639f3b1a5f0
Thanwar <60439635+Thanwar@users.noreply.github.com>
2022-08-04 01:35:42 +0500

    dummy Front End added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import './style.css';
+import './test.css';
+
+import Logo from './logo.png'
+import Trending from './imgages/trending.PNG'
+import Post from './imgages/posts.PNG'
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div id="nav">
+          <img id="navimg" src={Logo} height="40"/>
+          <h2 id="navtext">Reddit</h2>
+          <input id="navsearch" type="text" placeholder="Search" />
+          <button className="navbtn">Log In</button>
+          <button className="navbtn">Sign UP</button>
+        </div>
+        <div>
+          <h3 id="trendtxt">Trending today</h3>
+          <img id="trendimg" src={Trending} height="200"/>
+        </div>
+        <div>
+          <h3 id="trendtxt">Popular posts</h3>
+          <img id="trendimg" src={Post} height="500"/>
+        </div>
     </div>
   );
 }
